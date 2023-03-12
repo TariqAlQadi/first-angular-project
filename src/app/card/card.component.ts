60eaf71b1017e3a4d741fcb5b3f32f06eb0ecd2a
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-12 13:41:15 +0100

    add button funtions

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  like() {
+    alert('Your have liked this article!');
+  }
 
+  addToCard() {
+    alert('Your have added this article to your cart!');
+  }
 }
