278f17a90e7e7f5f48b001b2fd79021cc0d662c6
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-12 14:03:24 +0100

    made images and text dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  articleTexts = [
+    'These are super rare playing cards',
+    'Just 50 Decks ever made!',
+  ];
+
+  articleImages = ['assets/img/1.jpg', 'assets/img/2.jpg'];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() img: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
   like() {
     alert('Your have liked this article!');
   }
