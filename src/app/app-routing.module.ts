2a45113f4ffbb8c08fad49dfb9fc051c65b9ee0c
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-19 21:05:00 +0100

    smaller routing bugfixes .. dynameic routing still does not wor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CartComponent } from './cart/cart.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'articles', pathMatch: 'full' },
+import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
+
+const appRoutes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: '/allArticles', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: false, // <-- debugging purposes only
+      preloadingStrategy: SelectivePreloadingStrategyService,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
