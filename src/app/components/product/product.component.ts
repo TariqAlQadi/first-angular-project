9f22f3fd3548c0c1ffc8a4755778e47ac0b5235b
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-21 22:14:58 +0100

    added dynamic routing and some basic stylings

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,14 +10,55 @@ export class ProductComponent implements OnInit {
   name = '';
   id = '';
   description = '';
-  price = '';
+  price = 0;
   imageSource = '';
 
   constructor(private route: ActivatedRoute) {}
 
+  products = [
+    {
+      id: '1',
+      name: 'Cards',
+      description: 'cool',
+      price: 12.5,
+      imageSource: '/assets/img/1.jpg',
+    },
+    {
+      id: '2',
+      name: 'More cards',
+      description: 'very cool',
+      price: 43.5,
+      imageSource: '/assets/img/2.jpg',
+    },
+    {
+      id: '3',
+      name: 'Deck',
+      description: 'cool',
+      price: 99.5,
+      imageSource: '/assets/img/3.jpg',
+    },
+    {
+      id: '4',
+      name: 'Rare cards',
+      description: 'cool',
+      price: 32.5,
+      imageSource: '/assets/img/4.jpg',
+    },
+  ];
+
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    this.loadData(this.id);
   }
 
-  loadData() {}
+  loadData(id: string) {
+    const currentProduct = this.products.find((product) => id === product.id);
+
+    if (currentProduct) {
+      this.name = currentProduct.name;
+      this.description = currentProduct.description;
+      this.price = currentProduct.price;
+      this.imageSource = currentProduct.imageSource;
+    }
+  }
 }
