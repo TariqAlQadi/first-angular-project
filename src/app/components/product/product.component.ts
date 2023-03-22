72282a977bb624f5c922d2dc46b7baef8b0d5911
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-22 19:58:36 +0100

    added stock and global service for data and functions

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GlobalService } from 'src/app/global.service';
 
 @Component({
   selector: 'app-product',
@@ -12,39 +13,12 @@ export class ProductComponent implements OnInit {
   description = '';
   price = 0;
   imageSource = '';
+  stock = 0;
 
-  constructor(private route: ActivatedRoute) {}
-
-  products = [
-    {
-      id: '1',
-      name: 'Cards',
-      description: 'cool',
-      price: 12.5,
-      imageSource: '/assets/img/1.jpg',
-    },
-    {
-      id: '2',
-      name: 'More cards',
-      description: 'very cool',
-      price: 43.5,
-      imageSource: '/assets/img/2.jpg',
-    },
-    {
-      id: '3',
-      name: 'Deck',
-      description: 'cool',
-      price: 99.5,
-      imageSource: '/assets/img/3.jpg',
-    },
-    {
-      id: '4',
-      name: 'Rare cards',
-      description: 'cool',
-      price: 32.5,
-      imageSource: '/assets/img/4.jpg',
-    },
-  ];
+  constructor(
+    private route: ActivatedRoute,
+    private globalService: GlobalService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -52,13 +26,16 @@ export class ProductComponent implements OnInit {
   }
 
   loadData(id: string) {
-    const currentProduct = this.products.find((product) => id === product.id);
+    const currentProduct = this.globalService
+      .getProducts()
+      .find((product) => id === product.id);
 
     if (currentProduct) {
       this.name = currentProduct.name;
       this.description = currentProduct.description;
       this.price = currentProduct.price;
       this.imageSource = currentProduct.imageSource;
+      this.stock = currentProduct.stock;
     }
   }
 }
---
+++ src/app/global.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GlobalService {
+  constructor() {}
+
+  //global datasets
+  private products: any[] = [
+    {
+      id: '1',
+      name: 'Cards',
+      description: 'cool',
+      price: 12.5,
+      imageSource: '/assets/img/1.jpg',
+      stock: 3,
+    },
+    {
+      id: '2',
+      name: 'More cards',
+      description: 'very cool',
+      price: 43.5,
+      imageSource: '/assets/img/2.jpg',
+      stock: 3,
+    },
+    {
+      id: '3',
+      name: 'Deck',
+      description: 'cool',
+      price: 99.5,
+      imageSource: '/assets/img/3.jpg',
+      stock: 3,
+    },
+    {
+      id: '4',
+      name: 'Rare cards',
+      description: 'cool',
+      price: 32.5,
+      imageSource: '/assets/img/4.jpg',
+      stock: 3,
+    },
+  ];
+  private cart: any[] = [];
+  private wish: any[] = [];
+
+  //global injectable functions
+  getProducts(): any[] {
+    return this.products;
+  }
+
+  addCart(product: any) {
+    this.cart.push(product);
+  }
+
+  addWish(product: any) {
+    this.wish.push(product);
+  }
+
+  clearCart() {
+    this.cart = [];
+  }
+
+  clearWish() {
+    this.wish = [];
+  }
+
+  removeStock(id: any) {
+    const foundProduct = this.products.find((product) => {
+      product.id === id;
+    });
+    if (foundProduct.stock > 0) {
+      foundProduct.stock = foundProduct.stock--;
+    } else {
+      console.log('no more in stock!');
+    }
+  }
+}
