7f1faa86713855284e2e41ed8d21f67d459bd9a7
Tariq Al Qadi <tariqalqadi@gmx.de>
2023-03-26 16:01:28 +0200

    added ngFor for product cards

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/global.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit {
+  constructor(private globalService: GlobalService) {}
 
+  products = this.globalService.getProducts();
+
+  ngOnInit(): void {}
 }
